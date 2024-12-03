d3d32820e6a44e6adac341b4e855f501fa372afb
Keks <keks@htmlacademy.ru>
2024-12-03 21:18:16 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/util.js
@@ -0,0 +1,43 @@
+
+
+//Функция по возврату рандомного числа в пределах диапазона включительно
+const getRandomInteger = (min, max) => {
+  const minInteger = Math.ceil(Math.min(min, max));
+  const maxInteger = Math.floor(Math.max(min, max));
+  const randomInteger = Math.random() * (maxInteger - minInteger + 1) + minInteger;
+  return Math.floor(randomInteger);
+};
+getRandomInteger(1, 25);
+
+
+//Функция по возврату рандомного элемента маассива в пределах диапазона включительно
+function getRandomElementFromArray (array) {
+  const i = getRandomInteger(0, array.length - 1);
+  return array[i];
+}
+getRandomElementFromArray([1,2,3,4]);
+
+//Функция по возврату неповторяющегося числа в пределах диапазона включительно
+function getRandomIdFromRangeGenerator(min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+const randomIdFromRangeGenerator = getRandomIdFromRangeGenerator(1, 25);
+const randomIdFromRangeGenerator2 = getRandomIdFromRangeGenerator(1, 25);
+
+
+//Функция-проверка на соответствие ESC
+const isEscKey = (evt) => evt.key === 'Escape';
+
+export { getRandomInteger, getRandomElementFromArray, getRandomIdFromRangeGenerator, isEscKey };
